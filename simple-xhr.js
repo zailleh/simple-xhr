6d11c8caaaed1f523651187a24e175aaca4473f5
xael <tim.caldwell@einti.com>
2018-07-29 16:28:21 +1000

    added self-calling function to create a new instances of the function so that concurrent calls don't interfere with each other

--- simple-xhr.js
+++ simple-xhr.js
@@ -1,34 +1,35 @@
 const simpleXhr = function ( method, url, dataType ) {
-    // set up this object's methods and properties
-	simpleXhr.callbacks = [];
-    simpleXhr.xhr = new XMLHttpRequest();
-    simpleXhr.responseType = dataType.toLowerCase();
-    
-    // function used to call each attached callback function
-    simpleXhr.execCallbacks = function (e) {
-        //extract response text
-        let response = e.target.responseText; 
-        if (this.responseType === 'json') response = JSON.parse(response);
-        //execute supplied callbacks
+    return new function sXhr( method, url, dataType ) {
+        // set up this object's methods and properties
+        this.callbacks = [];
+        this.xhr = new XMLHttpRequest();
+        this.responseType = dataType.toLowerCase();
+        
+        // function used to call each attached callback function
+        this.execCallbacks = function (e) {
+            //extract response text
+            let response = e.target.responseText; 
+            if (this.responseType === 'json') response = JSON.parse(response);
+            //execute supplied callbacks
 
-        this.callbacks.forEach( (fn) => fn(response) ); 
-    }
-    simpleXhr.execCallbacks = simpleXhr.execCallbacks.bind(simpleXhr); //bind to this function
+            this.callbacks.forEach( (fn) => fn(response) ); 
+        }
+        this.execCallbacks = this.execCallbacks.bind(this); //bind to this function
 
-    // set .done method up
-	simpleXhr.done = function( callback ) {
-        this.callbacks.push(callback); //store multiple callbacks in an array
-        return this; // return this so we can chain additional .done's
-    }
+        // set .done method up
+        this.done = function( callback ) {
+            this.callbacks.push(callback); //store multiple callbacks in an array
+            return this; // return this so we can chain additional .done's
+        }
 
-    
-    xhr = simpleXhr.xhr
-	xhr.addEventListener('loadend', simpleXhr.execCallbacks);
-    
-    // execute the xhr
-    xhr.open( method, url );
-    xhr.send();
+        
+        xhr = this.xhr
+        xhr.addEventListener('loadend', this.execCallbacks);
+        
+        // execute the xhr
+        xhr.open( method, url );
+        xhr.send();
 
-    // return this function as an object so we can call the .done method.
-	return simpleXhr;
+        return this
+    }(method, url, dataType);
 };
